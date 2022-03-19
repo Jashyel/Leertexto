8b71d262282f643c81cf67af3a287cb7870ed5f5
Jaziel <99679608+Jashyel@users.noreply.github.com>
2022-03-18 19:19:32 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+String line;
+try
+{
+    //Pass the file path and file name to the StreamReader constructor
+    StreamReader sr = new StreamReader("C:\\Users\\cid\\Documents\\Sample.txt");
+    //Read the first line of text
+    line = sr.ReadLine();
+    //Continue to read until you reach end of file
+    while (line != null)
+    {
+        //write the line to console window
+        Console.WriteLine(line);
+        //Read the next line
+        line = sr.ReadLine();
+    }
+    //close the file
+    sr.Close();
+    Console.ReadLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("Executing finally block.");
+}
